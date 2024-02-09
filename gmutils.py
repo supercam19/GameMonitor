42aa41423f79af8b89afa11d54ecd34f948a1cd7
Supercam19 <79729769+supercam19@users.noreply.github.com>
2024-02-08 21:49:55 -0500

    Initial commit

---
+++ gmutils.py
@@ -0,0 +1,135 @@
+import json
+import os
+
+import customtkinter
+import requests
+import win32api
+import ctypes
+
+from screeninfo import screeninfo
+
+
+def json_read_safe(file, key):
+    json_file = json.load(file)
+    if key in json_file:
+        return json_file[key]
+
+
+def check_files():
+    os.chdir(".")
+    if not os.path.isfile("displayz.exe"):
+        # print("displayz.exe not found, downloading...")
+        url = "https://github.com/michidk/displayz/releases/download/0.1.0/displayz.exe"
+        try:
+            r = requests.get(url, allow_redirects=True)
+
+            with open("displayz.exe", "wb") as f:
+                f.write(r.content)
+            # print("displayz.exe downloaded")
+        except:
+            pass
+    if not os.path.isfile("settings.json"):
+        with open("settings.json", "w") as f:
+            f.write('{"default_monitor": 0, "games": []}')
+    if not os.path.isfile("icon.ico"):
+        url = "tbd"
+        try:
+            r = requests.get(url, allow_redirects=True)
+
+            with open("icon.ico", "wb") as f:
+                f.write(r.content)
+        except:
+            pass
+
+
+def save_settings(settings):
+    with open("settings.json", "w") as f:
+        json.dump(settings, f, indent=4)
+
+
+def load_settings():
+    with open("settings.json", "r") as f:
+        return json.load(f)
+
+
+def getFileProperties(fname):
+    # https://stackoverflow.com/a/7993095
+    # Altered to return only ProductName or file name if no ProductName
+    """
+    Read all properties of the given file return them as a dictionary.
+    """
+    propNames = ('Comments', 'InternalName', 'ProductName',
+        'CompanyName', 'LegalCopyright', 'ProductVersion',
+        'FileDescription', 'LegalTrademarks', 'PrivateBuild',
+        'FileVersion', 'OriginalFilename', 'SpecialBuild')
+
+    props = {'FixedFileInfo': None, 'StringFileInfo': None, 'FileVersion': None}
+
+    try:
+        # backslash as parm returns dictionary of numeric info corresponding to VS_FIXEDFILEINFO struc
+        fixedInfo = win32api.GetFileVersionInfo(fname, '\\')
+        props['FixedFileInfo'] = fixedInfo
+        props['FileVersion'] = "%d.%d.%d.%d" % (fixedInfo['FileVersionMS'] / 65536,
+                fixedInfo['FileVersionMS'] % 65536, fixedInfo['FileVersionLS'] / 65536,
+                fixedInfo['FileVersionLS'] % 65536)
+
+        # \VarFileInfo\Translation returns list of available (language, codepage)
+        # pairs that can be used to retreive string info. We are using only the first pair.
+        lang, codepage = win32api.GetFileVersionInfo(fname, '\\VarFileInfo\\Translation')[0]
+
+        # any other must be of the form \StringfileInfo\%04X%04X\parm_name, middle
+        # two are language/codepage pair returned from above
+
+        strInfo = {}
+        for propName in propNames:
+            strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, propName)
+            ## print str_info
+            strInfo[propName] = win32api.GetFileVersionInfo(fname, strInfoPath)
+
+        props['StringFileInfo'] = strInfo
+    except:
+        return os.path.basename(fname).rstrip(".exe")
+    if props.get("StringFileInfo").get("ProductName") is None:
+        return os.path.basename(fname).rstrip(".exe")
+    return props.get("StringFileInfo").get("ProductName")
+
+
+def popup(title, message):
+    ctypes.windll.user32.MessageBoxW(0, message, title, 0)
+
+# might implement later
+# def get_installed_games():
+#     games = []
+#     subkey = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+#
+#     try:
+#         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, subkey) as key:
+#             for i in range(winreg.QueryInfoKey(key)[0]):
+#                 subkey_name = winreg.EnumKey(key, i)
+#                 with winreg.OpenKey(key, subkey_name) as subkey:
+#                     if subkey_name.startswith("Steam App"):
+#                         try:
+#                             display_name, _ = winreg.QueryValueEx(subkey, "InstallLocation")
+#                             games.append(display_name)
+#                         except FileNotFoundError:
+#                             print("Could not find install location")  # Some entries may not have a DisplayName
+#     except Exception as e:
+#         print(f"Error: {e}")
+#
+#     return games
+
+
+def reveal_monitor_ids(window):
+    # so broken
+    monitors = screeninfo.get_monitors()
+    popups = []
+    for monitor in monitors:
+        popups.append(customtkinter.CTkToplevel(window))
+        popups[-1].geometry(f"100x100+{monitor.x + 0.5*monitor.width - 50}+{monitor.y+ 0.5*monitor.height - 50}")
+        popups[-1].attributes("-topmost", True)
+        label = customtkinter.CTkLabel(popups[-1], text=f"Monitor ID: {len(popups) -1}", font=("Arial", 20))
+        label.pack(pady=10, side="top")
+        popups[-1].after(5000, popups[-1].destroy)
+    return popups
+
+
---
+++ main.py
@@ -0,0 +1,200 @@
+import sys
+
+sys.coinit_flags = 0 # Solve multithreading crash with WMI
+import customtkinter as ctk
+import wmi
+import screeninfo
+from gmutils import *
+import threading
+import infi.systray
+from tkinter import filedialog, PhotoImage
+from Tooltip import Tooltip
+
+# TODO
+# - Find a way to unblock the wmi process listener when the program quits
+# - Fix the canvas in the window
+
+
+class Window(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.games_list_len = 0
+        self.title("GameMonitor")
+        self.resizable(False, False)
+        self.protocol("WM_DELETE_WINDOW", self.withdraw)
+        self.iconbitmap("icon.ico")
+        self.visible = True
+        self.toplevels = []
+
+        # Place window in the middle of the screen
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        window_width = self.winfo_width()
+        window_height = self.winfo_height()
+        x = (screen_width / 2) - (window_width / 2)
+        y = (screen_height / 2) - (window_height / 2)
+        self.geometry('500x400+%d+%d' % (x, y + 30))
+
+        self.title_frame = ctk.CTkFrame(self)
+        self.title_frame.pack(side='top', fill='x')
+
+        #self.reveal_monitors = ctk.CTkButton(self.title_frame, text="\U0001F5B5", command=self.reveal_monitors,
+        #                                     fg_color="#2b2b2b",
+        #                                     hover_color="#333333", width=20, font=("Arial", 20), corner_radius=8)
+        #self.reveal_monitors.pack(padx=10, pady=5, side='left')
+        #self.reveal_monitors_tt = Tooltip(self.reveal_monitors, "Reveal monitors IDs")
+
+        self.title_label = ctk.CTkLabel(self.title_frame, text="GameMonitor", font=("Arial", 20, "bold"))
+        self.title_label.pack(pady=10, side='left', padx=(200, 0))
+
+        self.add_game = ctk.CTkButton(self.title_frame, text="+", command=lambda: add_game(self), fg_color="#2b2b2b",
+                                      hover_color="#333333", width=20, font=("Arial", 20), corner_radius=8)
+        self.add_game.pack(padx=10, pady=5, side='right')
+        self.add_game_tt = Tooltip(self.add_game, "Add game")
+
+        self.games_list_frame = ctk.CTkScrollableFrame(self, width=500, height=340, fg_color="#2e2e2e", bg_color="#2e2e2e")
+        self.games_list_frame.pack(side="top", fill="both", expand=True)
+
+    def reveal_monitors(self):
+        self.toplevels = reveal_monitor_ids(self)
+        # gotta store in obj memory so they can kill themselves
+
+
+
+class Game:
+    def __init__(self, name, process_name, path, monitor):
+        self.name = name
+        self.process_name = process_name
+        self.path = path
+        self.game_fr = None
+        self.game_nm = None
+        self.game_monitor_select = None
+        self.monitor = monitor
+
+    def update_monitor(self, monitor):
+        self.monitor = self.game_monitor_select.get()
+        settings = load_settings()
+        for game in settings["games"]:
+            if game["path"] == self.path:
+                game["monitor"] = int(self.monitor[-1])
+                save_settings(settings)
+                break
+
+    def add_to_window(self, window):
+        colour = "#333333" if window.games_list_len % 2 == 0 else "#2b2b2b"
+        window.games_list_len += 1
+        self.game_fr = ctk.CTkFrame(window.games_list_frame, bg_color=colour, fg_color=colour)
+        self.game_fr.pack(fill="x")
+        self.game_nm = ctk.CTkLabel(self.game_fr, text=self.name, font=("Arial", 16))
+        self.game_nm.pack(side="left", padx=10)
+        self.game_monitor_select = ctk.CTkOptionMenu(self.game_fr, values=["Monitor 0", "Monitor 1"], command=self.update_monitor)
+        self.game_monitor_select.pack(side="right", padx=10, pady=5)
+        self.game_monitor_select.set("Monitor " + str(self.monitor))
+
+
+class ProcessListener(threading.Thread):
+    def __init__(self, games):
+        super().__init__()
+        self.games = games
+        self.sleepy_time = False
+        self.c = None
+        self.process_watcher = None
+
+    def run(self):
+        process_names = [game.process_name for game in self.games]
+        self.c = wmi.WMI()
+        self.process_watcher = self.c.Win32_Process.watch_for("creation")
+        while not self.sleepy_time:
+            try:
+                new_process = self.process_watcher(timeout_ms=1000)
+                if new_process.Name in process_names:
+                    for game in self.games:
+                        if game.process_name == new_process.Name:
+                            result = set_monitor(game.monitor)
+                            if result:
+                                self._wait_for_process_end(game)
+            except wmi.x_wmi_timed_out:
+                continue
+
+    def _wait_for_process_end(self, game):
+        if not self.c: raise Exception("WMI not initialized")
+        self.process_monitor = self.c.Win32_Process.watch_for("deletion")
+        while not self.sleepy_time:
+            try:
+                deleted_process = self.process_monitor(timeout_ms=1000)
+                if deleted_process.Name == game.process_name:
+                    set_monitor(default_monitor)
+                    return
+            except wmi.x_wmi_timed_out:
+                continue
+
+    def stop(self):
+        self.sleepy_time = True
+
+def set_monitor(monitor):
+    monitors = screeninfo.get_monitors()
+    if monitors[monitor].is_primary == False:
+        os.chdir(".")
+        try:
+            os.system(f"displayz.exe set-primary --id {monitor}")
+            # subprocess.call([f"displayz.exe set-primary --id {monitor}"])
+            return True
+        except:
+            return False
+    return False
+
+
+def spawn_window(window):
+    window.deiconify()
+    window.visible = True
+
+
+def add_game(window):
+    path = filedialog.askopenfilename(title="Select a game's executable file", filetypes=(("Game executable", "*.exe"),))
+    settings = load_settings()
+    for game in settings["games"]:
+        if game["path"] == path:
+            popup("Game already added", "This game has already been added to the list.")
+            return
+    name = getFileProperties(path)
+    filename = os.path.basename(path)
+    game = Game(name, filename, path, 0)
+    game.add_to_window(window)
+
+    settings["games"].append({"name": name, "process_name": filename, "path": path, "monitor": 0})
+    save_settings(settings)
+
+
+def main(open_window):
+    check_files()
+    ctk.set_appearance_mode("system")
+    ctk.deactivate_automatic_dpi_awareness() # DPI awareness breaks the scrolling frame
+    games_data = load_settings().get("games")
+    games = []
+    for game in games_data:
+        games.append(Game(game.get("name"), game.get("process_name"), game.get("path"), game.get("monitor")))
+    listener = ProcessListener(games)
+    listener.start()
+    window = Window()
+
+    for game in games:
+        game.add_to_window(window)
+
+    if not open_window:
+        window.withdraw()
+        window.visible = False
+
+    menu_options = (("Settings", None, lambda e: spawn_window(window)),)
+    systray = infi.systray.SysTrayIcon("icon.ico", "GameMonitor", menu_options, on_quit=lambda e: window.quit())
+    systray.start()
+
+    window.mainloop()
+    # stop the other threads
+    listener.stop()
+    listener.join()
+
+
+if __name__ == "__main__":
+    default_monitor = 0
+    show_on_startup = False if "--startup" in sys.argv else True
+    main(show_on_startup)
